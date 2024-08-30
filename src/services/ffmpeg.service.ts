b84b1440af0fcba112e85c31110d8388094bc44c
gf2004 <dotsenk20034ua@gmail.com>
2024-08-30 21:20:29 +0300

    2# fix: ffmpeg error chrome android

--- src/services/ffmpeg.service.ts
+++ src/services/ffmpeg.service.ts
@@ -72,7 +72,7 @@ export class FFmpegService {
                videoCompress.value.name = file.name;
                videoCompress.value.size_compressed = data.length;
 
-               videoCompress.value.video_blob = URL.createObjectURL(new Blob([data], { type: 'video/mp4' }));
+               videoCompress.value.video_blob = URL.createObjectURL(new File([data], 'video.mp4', { type: 'video/mp4' }));
           } catch (err) {
                console.error('Error during compression:', err);
           } finally {
