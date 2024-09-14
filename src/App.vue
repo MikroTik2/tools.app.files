6c6ee17f5d166ca84a4f5938abc11f0d7b8e75fa
gf2004 <dotsenk20034ua@gmail.com>
2024-09-14 08:39:30 +0300

    change: meta tags

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
 <template>
      <router-view />
-
-     <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9894617939719009"
-          crossorigin="anonymous"
-     ></script>
-     <!-- sidebar -->
-     <ins
-          class="adsbygoogle"
-          style="display: block"
-          data-ad-client="ca-pub-9894617939719009"
-          data-ad-slot="3513846043"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-     ></ins>
-     <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-     </script>
 </template>
